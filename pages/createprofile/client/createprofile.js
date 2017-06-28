15837d2a71644274667e53dee562a5effc19ccaf
kyzzzhang <kyzzzhang@brandeis.edu>
2017-06-28 10:07:02 -0400

    modified

--- pages/createprofile/client/createprofile.js
+++ pages/createprofile/client/createprofile.js
@@ -7,7 +7,14 @@ Template.createprofile.events({
     event.preventDefault();
     const name=instance.$('#name').val();
     const career=instance.$('#career').val();
-    const gender=instance.$('#gender').val();
+    var gender="";
+    if($('input[id="male"]').is(':checked')){
+      gender="male";
+    }else if($('input[id="female"]').is(':checked')){
+      gender="female";
+    }else{
+      gender="other";
+    };
     const havevehicle=instance.$('#vehicle').val();
     const otherinfo=instance.$('#otherinfo').val();
     console.log('adding '+name);
@@ -36,11 +43,17 @@ Template.createprofile.events({
       return person.owner==Meteor.userId();
     }
   })
+
 Template.showprofile.events({
   'click #erase2'(elt,instance){
     console.dir(this);
     console.log(this.person._id);
     Meteor.call('newuser.remove',this.person);
-    //Friends.remove(this.person._id);
   }
+  'click #updateUsername1'(elt,instance){
+    const name=instance.$('#usernameUpdate1').val();
+    console.log('modifying '+name);
+    instance.$('#usernameUpdate1').val("");
+    Meteor.call('allusers.updateName', this.person._id, name)
+  },
 })
